5ac41f56667f2f77b5eb52e16a4fd2c566e4d748
torreslucs23 <torreslucs23@gmail.com>
2025-06-14 18:30:55 -0300

    added transcriptor

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import asyncio
 from services.youtube_loader import load_youtube_transcript
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 async def main():
-    url = "https://www.youtube.com/watch?v=AmsaaNFJ_Sk"
-    docs = await load_youtube_transcript(url)
-    print(docs)
+    url = "https://www.youtube.com/watch?v=68O8ez0eEGE"
+    docs =  await load_youtube_transcript(url)
+    logger.info(docs[0].page_content)
 
 if __name__ == "__main__":
     asyncio.run(main())
