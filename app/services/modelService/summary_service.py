a539be7b96881d92728073784e84c64594ebe529
joao_feitoza <joao_feitoza@rastrosystem.com.br>
2025-06-28 17:10:18 -0300

    avoid resource consumption if users send video without captions

--- app/services/modelService/summary_service.py
+++ app/services/modelService/summary_service.py
@@ -1,15 +1,21 @@
+from fastapi import HTTPException
 from app.services.modelService.create_model import create_llm
 from app.services.youtube_loader import load_youtube_transcript
 from app.services.modelService.prompt_pipeline import build_summary_prompt
 from langchain_core.output_parsers import StrOutputParser
 
-async def summarize(url: str) -> str:
+async def summarize(url: str) -> str | dict:
     if not url:
         return {"error": "URL is required"}
-    docs =  await load_youtube_transcript(url)
-    transcription = docs[0].page_content if docs else "não há transcrição para esse vídeo"
-    llm = await create_llm()
-    prompt = build_summary_prompt(transcription)
-    chain = prompt | llm | StrOutputParser()
-    response = chain.invoke({})
-    return response.strip()
+
+    docs = await load_youtube_transcript(url)
+    transcription = docs[0].page_content if docs else None
+    if transcription:
+        # llm = Groq_LLM()
+        llm = create_llm()
+        prompt = build_summary_prompt(transcription)
+        chain = prompt | llm | StrOutputParser()
+        response = chain.invoke({})
+        return response.strip()
+    else:
+        raise HTTPException(status_code=400, detail="Transcript not found for the provided URL")
